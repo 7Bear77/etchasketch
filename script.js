dfb0f04c708f830071e109026e2bfbc85078f616
bear <jeremydavisyoung@gmail.com>
2025-01-26 11:30:37 -0500

    Add functionality to restart the sketch and resize the pad Add button to refresh and resize Create functions to do the refresh and resize with limitations Attach those functions to the button Refactor code to move like items near each other; functions, constants, etc. for readability

--- script.js
+++ script.js
@@ -4,27 +4,60 @@
 // Divs ought to be flex-wrapped to fill entire container
 // Function needs to align the new divs in a grid layout using flexbox
 
-let initialSquares = 40;
+const initialSquares = 16;
 const containerDiv = document.getElementById("etchasketch");
-const gridArray = [];
+const clearScreenBtn = document.getElementById("clearScreenButton");
+const clearRow = document.getElementById("etchasketch")
 
-function makeGrids(size) {
-    for (let i = 0; i < size; i++) {
-        let column = document.createElement("div");
-        column.classList.add("column");
-        for (let j = 1; j <= size; j++) {
-            let row = document.createElement("div");
-            row.classList.add("row");
-            column.appendChild(row);
-        }
-        containerDiv.appendChild(column);
-    }
-}
+clearScreenBtn.addEventListener("click", clearScreen);
 
-console.log(gridArray);
 makeGrids(initialSquares);
 
-// create a function that will color the background of divs that are hovered over
+function makeGrids(size) {
+    if (size > 100 || size <=0) {
+        resize();
+        } else {
+            for (let i = 0; i < size; i++) {
+            let column = document.createElement("div");
+            column.classList.add("column");
+            for (let j = 1; j <= size; j++) {
+                let row = document.createElement("div");
+                row.classList.add("row");
+                column.appendChild(row);
+            }
+            containerDiv.appendChild(column);
+        }
+    }
+    allowColorChange();
+}
 
-const gridBox = document.querySelectorAll(".row");
-gridBox.forEach(div => div.addEventListener('mouseover', (e) => e.target.style.background = "black")); // took too much googling
+// create a function that will color the background of divs that are hovered over
+function allowColorChange() {
+    let gridBox = document.querySelectorAll(".row");
+    gridBox.forEach(div => div.addEventListener('mouseover', 
+        (e) => e.target.style.background = "black")); // took too much googling
+}
+// Create button that will alow user to clear the screen and grid, and select a new grid size
+// Create the button
+// Create a function that can clear the current grid
+// Create a function that will prompt the user to select a grid size
+// Use the propmted input to create a grid of that size
+// Attach both functions to the button
+
+
+
+function clearScreen() {
+    while (clearRow.firstChild) {
+        clearRow.removeChild(clearRow.firstChild);
+    }
+    resize();
+}
+
+function resize() {
+    let newSize = prompt("Pick a grid size up to 100 squares");
+    while (!/^[0-9]+$/.test(newSize)) {
+        alert("Try again");
+        newSize = prompt("Pick a grid size up to 100 squares");
+    }
+    makeGrids(newSize);
+}
