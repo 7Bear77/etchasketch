8818509b11529ceb7f2cb8989f4cb9bf06ebbc64
bear <jeremydavisyoung@gmail.com>
2025-01-26 10:41:13 -0500

    Add function to color while hovering

--- script.js
+++ script.js
@@ -4,21 +4,27 @@
 // Divs ought to be flex-wrapped to fill entire container
 // Function needs to align the new divs in a grid layout using flexbox
 
-let initialSquares = 16;
+let initialSquares = 40;
+const containerDiv = document.getElementById("etchasketch");
+const gridArray = [];
 
 function makeGrids(size) {
-    const containerDiv = document.getElementById("etchasketch");
     for (let i = 0; i < size; i++) {
         let column = document.createElement("div");
         column.classList.add("column");
         for (let j = 1; j <= size; j++) {
             let row = document.createElement("div");
             row.classList.add("row");
-            row.style.border = "1px solid red";
             column.appendChild(row);
         }
         containerDiv.appendChild(column);
     }
 }
 
+console.log(gridArray);
 makeGrids(initialSquares);
+
+// create a function that will color the background of divs that are hovered over
+
+const gridBox = document.querySelectorAll(".row");
+gridBox.forEach(div => div.addEventListener('mouseover', (e) => e.target.style.background = "black")); // took too much googling
