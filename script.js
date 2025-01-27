9d63550a5edae928d546a06240b0f050b25d6930
bear <jeremydavisyoung@gmail.com>
2025-01-27 09:09:42 -0500

    Add styling touches and extra features Add styling touches to design UI Add additional features, black only button

--- script.js
+++ script.js
@@ -3,14 +3,38 @@
 // For (num of squares) create (num of squares) divs
 // Divs ought to be flex-wrapped to fill entire container
 // Function needs to align the new divs in a grid layout using flexbox
+// create a function that will color the background of divs that are hovered over
+// Create button that will alow user to clear the screen and grid, and select a new grid size
+// Create the button
+// Create a function that can clear the current grid
+// Create a function that will prompt the user to select a grid size
+// Use the propmted input to create a grid of that size
+// Attach both functions to the button
+// Allow the user to have a color change effect
+// Store values in an array
+// Randomly select a value from the array to change the background
 
 const initialSquares = 16;
 const containerDiv = document.getElementById("etchasketch");
-const clearScreenBtn = document.getElementById("clearScreenButton");
+const resizeScreenBtn = document.getElementById("resizeScreenButton");
 const clearRow = document.getElementById("etchasketch")
+const colorBtn = document.getElementById("newColorButton");
+const blackBtn = document.getElementById("backToBlack");
 
-clearScreenBtn.addEventListener("click", clearScreen);
+resizeScreenBtn.addEventListener("click", clearScreen);
+blackBtn.addEventListener("click", () => {
+    randomColors = false;
+    clearScreen();
+});
 
+const colors = ["black", "red", "orange", "yellow", "green", "blue", "indigo", "magenta", "cyan", "lightred"];
+
+let randomColors = false;
+
+colorBtn.addEventListener("click", () => {
+    randomColors = true;
+    clearScreen();
+})
 makeGrids(initialSquares);
 
 function makeGrids(size) {
@@ -31,21 +55,17 @@ function makeGrids(size) {
     allowColorChange();
 }
 
-// create a function that will color the background of divs that are hovered over
 function allowColorChange() {
-    let gridBox = document.querySelectorAll(".row");
-    gridBox.forEach(div => div.addEventListener('mouseover', 
-        (e) => e.target.style.background = "black")); // took too much googling
-}
-// Create button that will alow user to clear the screen and grid, and select a new grid size
-// Create the button
-// Create a function that can clear the current grid
-// Create a function that will prompt the user to select a grid size
-// Use the propmted input to create a grid of that size
-// Attach both functions to the button
-
-
-
+    if (randomColors) {
+        let gridBox = document.querySelectorAll(".row");
+        gridBox.forEach(div => div.addEventListener('mouseover',
+            (e) => e.target.style.background = colors[Math.floor(Math.random() * colors.length)]));
+        } else {
+            let gridBox = document.querySelectorAll(".row");
+            gridBox.forEach(div => div.addEventListener('mouseover',
+                (e) => e.target.style.background = colors[0]));
+            }
+        }
 function clearScreen() {
     while (clearRow.firstChild) {
         clearRow.removeChild(clearRow.firstChild);
